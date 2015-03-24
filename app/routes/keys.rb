9cf91de61341d3e4e68bdd40a48a64635f2c359f
Mike Arnold <dharmamike@gmail.com>
2015-03-24 17:23:15 -0500

    adding description and required attributes to schema keys

--- app/routes/keys.rb
+++ app/routes/keys.rb
@@ -5,7 +5,9 @@ module Lorry
       namespace '/keys' do
         get do
           schema = Lorry::Models::ComposeValidator.schema
-          @keys = schema['mapping']['=']['mapping'].keys
+          @keys = schema['mapping']['=']['mapping'].map do |validation|
+            { validation[0] => validation[1].keep_if { |k, v| %w(desc required).include?(k) } }
+          end
           status 200
           @keys.to_json
         end
--- spec/routes/keys_spec.rb
+++ spec/routes/keys_spec.rb
@@ -15,6 +15,20 @@ describe Lorry::Routes::Keys do
       expect(JSON.parse(response.body)).not_to be_empty
     end
 
+    it 'returns an array of hashes' do
+      response = get '/keys'
+      expect(JSON.parse(response.body)).to all(be_a Hash)
+    end
+
+    it 'returns an array of hashes, each having a description' do
+      response = get '/keys'
+      expect(JSON.parse(response.body)).to all( satisfy { |h| h.each_value { |v| v.has_key?('desc') } })
+    end
+
+    it 'returns an array of hashes, each indicating whether or not it is required' do
+      response = get '/keys'
+      expect(JSON.parse(response.body)).to all( satisfy { |h| h.each_value { |v| v.has_key?('required') } })
+    end
   end
 
 end
