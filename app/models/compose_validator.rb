e0aea84fed147ab2ac030d1f0a1558e1d53c6932
Mike Arnold <dharmamike@gmail.com>
2015-04-23 16:17:50 -0500

    Adding whitelist validation for several keys

--- app/models/compose_validator.rb
+++ app/models/compose_validator.rb
@@ -22,20 +22,88 @@ module Lorry
         when 'Image'
           value = value.to_s.strip
           if value.empty?
-            errors << Lorry::Errors::ComposeValidationWarning.new("image should not be blank", path)
+            errors << Lorry::Errors::ComposeValidationWarning.new('image should not be blank', path)
           end
         when 'Link'
           value = value.to_s.split(':').first
           unless services.include?(value)
             errors << Kwalify::ValidationError.new("#{value} references an undefined service", path)
           end
-        when 'Environment', 'DNS', 'Search'
+        when 'EnvFile', 'DNSSearch'
           unless value.is_a?(String) || value.is_a?(Array)
             errors << Kwalify::ValidationError.new('value is not a string or sequence', path)
+          end
+        when 'Port'
+          if value.is_a?(Array) && !value.empty?
+            value.each { |port| validate_port_format(port, path, errors) }
+          end
+        when 'Expose'
+          if value.is_a?(Array) && !value.empty?
+            value.each { |expose| validate_expose_format(expose, path, errors) }
+          end
+        when 'DNS'
+          if value.is_a?(String) && value.empty?
+            return
+          elsif value.is_a?(String)
+            validate_dns_format(value, path, errors)
+          elsif value.is_a?(Array)
+            value.each { |dns| validate_dns_format(dns, path, errors) }
+          else
+            errors << Kwalify::ValidationError.new('value is not a string or sequence', path)
+          end
+        when 'Net'
+          return if value.is_a?(String) && value.empty?
+          unless value.start_with?("bridge", "none", "container:", "host")
+            errors << Lorry::Errors::ComposeValidationWarning.new('Invalid value', path)
+          end
+        when 'Privileged', 'TTY'
+          return if value.is_a?(String) && value.empty?
+          unless %w(true false).include? value
+            errors << Lorry::Errors::ComposeValidationWarning.new('Invalid value', path)
+          end
+        when 'Restart'
+          return if value.is_a?(String) && value.empty?
+          unless value.start_with?("no", "on-failure:", "always")
+            errors << Lorry::Errors::ComposeValidationWarning.new('Invalid value', path)
           end
         end
       end
 
+      private
+
+      def validate_port_format(value, path, errors)
+        valid = false
+        octet = /\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}/
+        port = /\d{2,6}/
+
+        parts = value.split(':')
+        case parts.size
+        when 3
+          valid = octet.match(parts[0]) && port.match(parts[1]) && port.match(parts[2])
+        when 2
+          valid = port.match(parts[0]) && port.match(parts[1])
+        when 1
+          valid = port.match(parts[0])
+        end
+
+        unless valid
+          errors << Lorry::Errors::ComposeValidationWarning.new('Invalid port format', path)
+        end
+      end
+
+      def validate_expose_format(value, path, errors)
+        port = /\d{2,6}/
+        unless port.match(value)
+          errors << Lorry::Errors::ComposeValidationWarning.new('Invalid expose format', path)
+        end
+      end
+
+      def validate_dns_format(value, path, errors)
+        octet = /\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}/
+        unless octet.match(value)
+          errors << Lorry::Errors::ComposeValidationWarning.new('Invalid DNS format', path)
+        end
+      end
     end
   end
 end
--- spec/models/compose_validator_spec.rb
+++ spec/models/compose_validator_spec.rb
@@ -87,29 +87,215 @@ describe ComposeValidator do
 
     end
 
-    context 'when enforcing the Environment rule' do
+    context 'when enforcing the EnvFile rule' do
 
       let(:errors) { [] }
-      let(:rule) { double('rule', name: 'Environment') }
+      let(:rule) { double('rule', name: 'EnvFile') }
       let(:path) { [] }
 
-      it 'adds an error when the environment is not a string or array' do
+      it 'adds an error when the env_file value is not a string or array' do
         subject.validate_hook({}, rule, path, errors)
         expect(errors.first.message).to eq 'value is not a string or sequence'
       end
 
-      it 'does not add an error when the environment is a string' do
-        subject.validate_hook('foo=bar', rule, path, errors)
+      it 'does not add an error when the env_file value is a string' do
+        subject.validate_hook('./', rule, path, errors)
         expect(errors).to be_empty
       end
 
-      it 'does not add an error when the environment is an array' do
-        subject.validate_hook(['foo=bar'], rule, path, errors)
+      it 'does not add an error when the env_file value is an array' do
+        subject.validate_hook(['./'], rule, path, errors)
         expect(errors).to be_empty
       end
 
     end
 
+    context 'when enforcing the DNSSearch rule' do
+
+      let(:errors) { [] }
+      let(:rule) { double('rule', name: 'DNSSearch') }
+      let(:path) { [] }
+
+      it 'adds an error when the dns_search value is not a string or array' do
+        subject.validate_hook({}, rule, path, errors)
+        expect(errors.first.message).to eq 'value is not a string or sequence'
+      end
+
+      it 'does not add an error when the dns_search value is a string' do
+        subject.validate_hook('www.example.com', rule, path, errors)
+        expect(errors).to be_empty
+      end
+
+      it 'does not add an error when the dns_search value is an array' do
+        subject.validate_hook(['www.example.com'], rule, path, errors)
+        expect(errors).to be_empty
+      end
+
+    end
+
+    context 'when enforcing the Port rule' do
+
+      let(:errors) { [] }
+      let(:rule) { double('rule', name: 'Port') }
+      let(:path) { [] }
+
+      it 'adds a warning when the value(s) are not in the proper format' do
+        subject.validate_hook(['not the right format'], rule, path, errors)
+        expect(errors.first).to be_a Lorry::Errors::ComposeValidationWarning
+        expect(errors.first.message).to eq 'Invalid port format'
+      end
+
+      it 'does not add a warning when the value is in the ip:hostPort:containerPort format' do
+        subject.validate_hook(['127.0.0.1:1234:1234'], rule, path, errors)
+        expect(errors).to be_empty
+      end
+
+      it 'does not add a warning when the value is in the hostPort:containerPort format' do
+        subject.validate_hook(['1234:1234'], rule, path, errors)
+        expect(errors).to be_empty
+      end
+
+      it 'does not add a warning when the value is in the containerPort format' do
+        subject.validate_hook(['1234'], rule, path, errors)
+        expect(errors).to be_empty
+      end
+    end
+
+  end
+
+  context 'when enforcing the Expose rule' do
+
+    let(:errors) { [] }
+    let(:rule) { double('rule', name: 'Expose') }
+    let(:path) { [] }
+
+    it 'adds a warning when the value(s) are not in the proper format' do
+      subject.validate_hook(['not the right format'], rule, path, errors)
+      expect(errors.first).to be_a Lorry::Errors::ComposeValidationWarning
+      expect(errors.first.message).to eq 'Invalid expose format'
+    end
+
+    it 'does not add a warning when the value is in the proper format' do
+      subject.validate_hook(['1234'], rule, path, errors)
+      expect(errors).to be_empty
+    end
+  end
+
+  context 'when enforcing the Net rule' do
+
+    let(:errors) { [] }
+    let(:rule) { double('rule', name: 'Net') }
+    let(:path) { [] }
+
+    it 'adds a warning when the value does not start with "bridge", "none", "container:", or "host"' do
+      subject.validate_hook('other', rule, path, errors)
+      expect(errors.first).to be_a Lorry::Errors::ComposeValidationWarning
+      expect(errors.first.message).to eq 'Invalid value'
+    end
+
+    it 'does not add a warning when the value starts with "bridge", "none", "container:", or "host"' do
+      subject.validate_hook('bridge: blah', rule, path, errors)
+      expect(errors).to be_empty
+
+      subject.validate_hook('none', rule, path, errors)
+      expect(errors).to be_empty
+
+      subject.validate_hook('container: blah', rule, path, errors)
+      expect(errors).to be_empty
+
+      subject.validate_hook('host: blah', rule, path, errors)
+      expect(errors).to be_empty
+    end
+
+  end
+
+  context 'when enforcing the DNS rule' do
+
+    let(:errors) { [] }
+    let(:rule) { double('rule', name: 'DNS') }
+    let(:path) { [] }
+
+    it 'adds a warning when the value(s) are not in the proper format' do
+      subject.validate_hook(['not the right format'], rule, path, errors)
+      expect(errors.first).to be_a Lorry::Errors::ComposeValidationWarning
+      expect(errors.first.message).to eq 'Invalid DNS format'
+    end
+
+    it 'does not add a warning when the value is in the proper format' do
+      subject.validate_hook(['8.8.8.8'], rule, path, errors)
+      expect(errors).to be_empty
+    end
+
+    it 'adds an error when the value is not a string or array' do
+      subject.validate_hook({}, rule, path, errors)
+      expect(errors.first.message).to eq 'value is not a string or sequence'
+    end
+
+  end
+
+  context 'when enforcing the Privileged rule' do
+
+    let(:errors) { [] }
+    let(:rule) { double('rule', name: 'Privileged') }
+    let(:path) { [] }
+
+    it 'adds a warning when the value is not "true" or "false"' do
+      subject.validate_hook('other', rule, path, errors)
+      expect(errors.first.message).to eq 'Invalid value'
+    end
+
+    it 'does not add a warning when the value is "true" or "false"' do
+      subject.validate_hook('true', rule, path, errors)
+      expect(errors).to be_empty
+
+      subject.validate_hook('false', rule, path, errors)
+      expect(errors).to be_empty
+    end
+  end
+
+  context 'when enforcing the Restart rule' do
+
+    let(:errors) { [] }
+    let(:rule) { double('rule', name: 'Restart') }
+    let(:path) { [] }
+
+    it 'adds a warning when the value does not start with "no", "on-failure:", or "always"' do
+      subject.validate_hook('other', rule, path, errors)
+      expect(errors.first).to be_a Lorry::Errors::ComposeValidationWarning
+      expect(errors.first.message).to eq 'Invalid value'
+    end
+
+    it 'does not add a warning when the value starts with "no", "on-failure:", or "always"' do
+      subject.validate_hook('no', rule, path, errors)
+      expect(errors).to be_empty
+
+      subject.validate_hook('on-failure: meh.', rule, path, errors)
+      expect(errors).to be_empty
+
+      subject.validate_hook('always', rule, path, errors)
+      expect(errors).to be_empty
+    end
+  end
+
+  context 'when enforcing the TTY rule' do
+
+    let(:errors) { [] }
+    let(:rule) { double('rule', name: 'TTY') }
+    let(:path) { [] }
+
+    it 'adds a warning when the value is not "true" or "false"' do
+      subject.validate_hook('other', rule, path, errors)
+      expect(errors.first.message).to eq 'Invalid value'
+    end
+
+    it 'does not add a warning when the value is "true" or "false"' do
+      subject.validate_hook('true', rule, path, errors)
+      expect(errors).to be_empty
+
+      subject.validate_hook('false', rule, path, errors)
+      expect(errors).to be_empty
+    end
   end
 
 end
+
